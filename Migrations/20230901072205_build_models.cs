f6716ab339e3ad0bb6780a0c54f11bb92f7a741c
queens kisivuli <queenskisivuli@gmail.com>
2023-09-04 15:40:39 +0300

    database migrations init

---
+++ Migrations/20230901072205_build_models.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Event_management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class build_models : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    TicketAmount = table.Column<double>(type: "float", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventUser",
+                columns: table => new
+                {
+                    RegisteredEventsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RegisteredUsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventUser", x => new { x.RegisteredEventsId, x.RegisteredUsersId });
+                    table.ForeignKey(
+                        name: "FK_EventUser_Event_RegisteredEventsId",
+                        column: x => x.RegisteredEventsId,
+                        principalTable: "Event",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventUser_Users_RegisteredUsersId",
+                        column: x => x.RegisteredUsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventUser_RegisteredUsersId",
+                table: "EventUser",
+                column: "RegisteredUsersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventUser");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Migrations/20230901081036_newlist.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Event_management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class newlist : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventUser_Event_RegisteredEventsId",
+                table: "EventUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Event",
+                table: "Event");
+
+            migrationBuilder.RenameTable(
+                name: "Event",
+                newName: "Events");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Events",
+                table: "Events",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventUser_Events_RegisteredEventsId",
+                table: "EventUser",
+                column: "RegisteredEventsId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventUser_Events_RegisteredEventsId",
+                table: "EventUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Events",
+                table: "Events");
+
+            migrationBuilder.RenameTable(
+                name: "Events",
+                newName: "Event");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Event",
+                table: "Event",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventUser_Event_RegisteredEventsId",
+                table: "EventUser",
+                column: "RegisteredEventsId",
+                principalTable: "Event",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
